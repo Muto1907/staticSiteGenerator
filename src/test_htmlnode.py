601d3d3b14b2b3e7cb0f0f642ce9f3866b82f03b
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-09 22:16:45 +0200

    Implement htmlnode.py with unittests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,23 @@
+import unittest
+
+from htmlnode import *
+
+class TestHtmlNode(unittest.TestCase):
+    def test_props(self):
+        tmp = HTMLNode("h1", "Titleee", [HTMLNode("p")], {"href": "https://google.de",})
+        htm = tmp.props_to_html()
+        print(htm)
+        self.assertEqual(htm, ' href="https://google.de"')
+
+    def test_repr(self):
+        tmp = HTMLNode("h1", "Titleee", [HTMLNode("p")], {"href": "https://google.de",})
+        str = tmp.__repr__()
+        self.assertEqual(str, "HTMLNODE(h1, Titleee, [HTMLNODE(p, None, None, None)], {'href': 'https://google.de'})")
+
+
+    def test_props2(self):
+        tmp = HTMLNode("h1", "Titleee", [HTMLNode("p")], {"href": "https://google.de", "target": "blank", "animal": "cat"})
+        htm = tmp.props_to_html()
+        print(htm)
+        self.assertEqual(htm, ' href="https://google.de" target="blank" animal="cat"')
+
