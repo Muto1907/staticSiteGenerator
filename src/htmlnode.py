8edfe959ef4fb375cd708c37d21e5267b0ffa0f2
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-10 22:52:56 +0200

    Move leafnodes code inside html nodes file

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,19 @@ class HTMLNode:
         
     def __repr__(self):
         return f"HTMLNODE({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag = None, props = None):
+        super().__init__(tag, value, None, props )
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag == None:
+            return self.value
+        result = f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return result
+            
+    def __repr__(self):
+        return f"LEAFNODE({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,19 +3,36 @@ import unittest
 from htmlnode import *
 
 class TestHtmlNode(unittest.TestCase):
-    def test_props(self):
-        tmp = HTMLNode("h1", "Titleee", [HTMLNode("p")], {"href": "https://google.de",})
-        htm = tmp.props_to_html()
-        self.assertEqual(htm, ' href="https://google.de"')
-
     def test_repr(self):
         tmp = HTMLNode("h1", "Titleee", [HTMLNode("p")], {"href": "https://google.de",})
         str = tmp.__repr__()
         self.assertEqual(str, "HTMLNODE(h1, Titleee, [HTMLNODE(p, None, None, None)], {'href': 'https://google.de'})")
 
+    def test_propps(self):
+        tmp = HTMLNode("h1", "Titleee", [HTMLNode("p")], {"href": "https://google.de",})
+        htm = tmp.props_to_html()
+        self.assertEqual(htm, ' href="https://google.de"')
 
     def test_props2(self):
         tmp = HTMLNode("h1", "Titleee", [HTMLNode("p")], {"href": "https://google.de", "target": "blank", "animal": "cat"})
         htm = tmp.props_to_html()
         self.assertEqual(htm, ' href="https://google.de" target="blank" animal="cat"')
 
+    def test_constr(self):
+        tst = LeafNode("this is a heading","h1")
+        self.assertEqual("<h1>this is a heading</h1>", tst.to_html())
+
+    def test_props(self):
+        tst = LeafNode("this is a heading","h1", {"href": "https://google.de"})
+        self.assertEqual('<h1 href="https://google.de">this is a heading</h1>', tst.to_html())
+
+    def test_mult_props(self):
+        tst = LeafNode("this is a paragraph", "p", {"href": "https://moeway.com", "class": "texto"})
+        self.assertEqual('<p href="https://moeway.com" class="texto">this is a paragraph</p>', tst.to_html())
+
+    def test_no_tag(self):
+        tst = LeafNode("Hello za worldo!", None)
+        self.assertEqual("Hello za worldo!", tst.to_html())
+
+if __name__ == "__main__":
+    unittest.main()
