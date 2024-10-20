629b50cc144717452ca35557bf68dd5b334e969d
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-20 17:40:53 +0200

    Move extraction method into block_md and add template.html + index.md

--- src/block_MD.py
+++ src/block_MD.py
@@ -110,3 +110,13 @@ def text_to_children(text):
     for node in text_nodes:
         res.append(text_node_to_html_node(node))
     return res
+def extract_title(markdown):
+    blocks = markdown_to_blocks(markdown)
+    result = ""
+    for block in blocks:
+        if block.strip().startswith("# "):
+            result = block.strip()[2:]
+            break
+    if result == "":
+        raise Exception("No h1 heading found.")
+    return result
--- src/test_block_MD.py
+++ src/test_block_MD.py
@@ -190,3 +190,19 @@ this is paragraph text
                 html,
                 "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
             )
+    def test_heading_extraction(self):
+        heading = extract_title("# Hello")
+        self.assertEqual("Hello", heading)
+
+    def test_heading_extraction_multiple_blocks(self):
+        heading = extract_title('''
+```this is some code```
+                                
+                                ### This is an h3
+
+
+                                    # This is the h1!       
+
+                                some paragraph
+        ''')
+        self.assertEqual("This is the h1!", heading)
