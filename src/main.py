133f8fe55e1f621ce33f70155516604ddff25b68
Muto1907 <Mahmut.Cinar93@outlook.de>
2024-10-24 20:23:47 +0200

    Change abs path into rel

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from textnode import *
 from filecpy import *
 def main():
-    Cpy_Directory("/home/mahmut/workspace/github.com/Muto1907/staticSiteGenerator/static/", "/home/mahmut/workspace/github.com/Muto1907/staticSiteGenerator/public/")
-    generate_page("/home/mahmut/workspace/github.com/Muto1907/staticSiteGenerator/content/index.md", "/home/mahmut/workspace/github.com/Muto1907/staticSiteGenerator/template.html", "/home/mahmut/workspace/github.com/Muto1907/staticSiteGenerator/public/index.html")
+    Cpy_Directory("./static/", "./public/")
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 if __name__ == "__main__":
     main()
